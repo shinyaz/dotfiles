9e122e961b860e6f51789555f9a733cd9d858252
Shinya Tahara <shinya@fastmail.com>
2023-11-04 14:20:19 +0900

    update some fles for 1Password

--- README.md
+++ README.md
@@ -99,6 +99,9 @@ chmod 700 ~/.ssh
 cp $DOTFILES/ssh/config ~/.ssh/config
 chmod 600 ~/.ssh/config
 
+mkdir -p $XDG_CONFIG_HOME/1Password/ssh
+cp $DOTFILES/1Password/ssh/agent.toml $XDG_CONFIG_HOME/1Password/ssh/agent.toml
+
 ```
 
 ### ghq
--- ssh/install.sh
+++ ssh/install.sh
@@ -6,3 +6,6 @@ set -e
 chmod 700 ~/.ssh 
 cp ${DOTFILES}/ssh/config ~/.ssh/config
 chmod 600 ~/.ssh/config
+
+[ ! -e ${XDG_CONFIG_HOME}/1Password/ssh ] && mkdir -p ${XDG_CONFIG_HOME}/1Password/ssh
+cp ${DOTFILES}/1Password/ssh/agent.toml ${XDG_CONFIG_HOME}/1Password/ssh/agent.toml
